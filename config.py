2359c2816852b623fe6b823b8c38873b2557ceb9
TargetEric <nkansara1122@gmail.com>
2024-11-11 21:03:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,14 +30,14 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/CyberPixelPro/AviaxMusic",
-)
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
+# UPSTREAM_REPO = getenv(
+#     "UPSTREAM_REPO",
+#     "https://github.com/CyberPixelPro/AviaxMusic",
+# )
+# UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+# GIT_TOKEN = getenv(
+#     "GIT_TOKEN", None
+# )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NexGenBots")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NexGenSpam")
